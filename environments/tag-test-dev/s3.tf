2c8d37c26e18bf11d028ca4a87cc890e5efbad92
folarin oyenuga <folarin.oyenuga@digital.justice.gov.uk>
2026-01-27 13:16:15 +0000

    feat: add compliant S3 bucket to demonstrate passing checks

---
+++ environments/tag-test-dev/s3.tf
@@ -0,0 +1,20 @@
+# Compliant S3 bucket - inherits all required tags from provider default_tags
+resource "aws_s3_bucket" "compliant_bucket" {
+  bucket = "tag-test-compliant-bucket-demo"
+}
+
+resource "aws_s3_bucket_versioning" "compliant_bucket" {
+  bucket = aws_s3_bucket.compliant_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "compliant_bucket" {
+  bucket = aws_s3_bucket.compliant_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
